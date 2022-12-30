99fe2f14cca09aae0606f79537d797b4343c7d8e
Rajeev Singh Naruka <rajivsingh430@gmail.com>
2022-12-30 18:07:19 +0530

    helper scripts to scrape ProductHunt data

---
+++ analysis.py
@@ -0,0 +1,191 @@
+import pandas as pd
+
+df = pd.read_csv("/Users/rajeev/Downloads/products_async.csv")
+
+
+# > head -3 products_async.csv                                                                            ─╯
+# id,name,slug,tagline,shortenedUrl,commentsCount,createdAt,featuredAt,updatedAt,pricingType,topic_id,topic_name,topic_slug,redirectToProduct_id,redirectToProduct_slug,disabledWhenScheduled,votesCount,productState,thumbnailImageUuid
+# 325054,World Explorer by Insured Nomads,world-explorer-by-insured-nomads,Insurance meets travel tech for the global work revolution,/r/p/325054,84,2022-01-01T00:12:52-08:00,2022-01-01T00:12:52-08:00,2022-12-24T09:50:42-08:00,,249,Global Nomad,global-nomad,119745,insured-nomads,True,389,default,9bc773c8-1720-45b6-86f3-75d257338bc3.png
+# 325075,Tailwind Box Shadows,tailwind-box-shadows,Curated list of box shadows for your cards to stand out,/r/p/325075,25,2022-01-01T02:40:32-08:00,2022-01-01T02:40:32-08:00,2022-12-21T09:16:59-08:00,free,46,Productivity,productivity,472948,tailwind-box-shadows,True,203,default,f182991d-f034-43b8-8672-c5ec338ba09d.png
+
+
+def highest_voted_product(df):
+    df = df.sort_values("votesCount", ascending=False)
+    highest_vote_product = df.iloc[0]
+    print(
+        f"Product with highest votes: {highest_vote_product['name']} with {highest_vote_product['votesCount']} votes"
+    )
+
+
+def most_commented_product(df):
+    df = df.sort_values("commentsCount", ascending=False)
+    most_commented_product = df.iloc[0]
+    print(
+        f"Product with most comments: {most_commented_product['name']} with {most_commented_product['commentsCount']} comments"
+    )
+
+
+def top_10_topics(df):
+    top_df = df.groupby("topic_name").size().sort_values(ascending=False).head(10)
+    print(top_df)
+
+
+def top_20_words_in_tagline(df):
+    hash_table = {}
+
+    for tagline in df["tagline"]:
+        words = tagline.split(" ")
+        for word in words:
+            if word in hash_table:
+                hash_table[word] += 1
+            else:
+                hash_table[word] = 1
+
+    sorted_hash_table = sorted(hash_table.items(), key=lambda x: x[1], reverse=True)
+    # remove if count is <=5
+    sorted_hash_table = [x for x in sorted_hash_table if x[1] > 150]
+
+    print(sorted_hash_table)
+
+
+def dates_with_most_and_least_products(df):
+    hash_table = {}
+
+    for date in df["createdAt"]:
+        date = date.split("T")[0]
+        if date in hash_table:
+            hash_table[date] += 1
+        else:
+            hash_table[date] = 1
+
+    sorted_hash_table = sorted(hash_table.items(), key=lambda x: x[1], reverse=True)
+
+    print("Date with most products: ", sorted_hash_table[0])
+    print("Date with least products: ", sorted_hash_table[-1])
+
+
+def total_products(df):
+    print("Total products: ", len(df))
+
+
+def pricing_options_distribution(df):
+    pricing_options = df.groupby("pricingType").size()
+
+    print(pricing_options)
+
+    # print % of total products
+    for index, value in pricing_options.items():
+        print(f"{index} %: {value / len(df) * 100}")
+
+
+def comments_per_month(df):
+    """prints list of comments per month, sorted by month (2022-01-01T00:12:52-08:00 is the format)"""
+    hash_table = {}
+
+    for date in df["createdAt"]:
+        date = date.split("T")[0]
+        month = date.split("-")[1]
+        if month in hash_table:
+            hash_table[month] += 1
+        else:
+            hash_table[month] = 1
+
+    sorted_hash_table = sorted(hash_table.items(), key=lambda x: x[0])
+
+    print("Comments per month: ", sorted_hash_table)
+
+
+def product_with_most_comments_per_vote(df):
+    """prints product with most comments per vote"""
+    df["comments_per_vote"] = df["commentsCount"] / df["votesCount"]
+    df = df.sort_values("comments_per_vote", ascending=False)
+    most_commented_product_per_vote = df.iloc[0]
+    print(
+        f"Product with most comments per vote: {most_commented_product_per_vote['name']} with {most_commented_product_per_vote['comments_per_vote']} comments per vote"
+    )
+
+
+def product_with_largest_difference_between_create_and_update_date(df):
+    """prints product with largest difference between create and update date"""
+    df["create_date"] = pd.to_datetime(df["createdAt"])
+    df["update_date"] = pd.to_datetime(df["updatedAt"])
+    df["time_diff"] = df["update_date"] - df["create_date"]
+    df = df.sort_values("time_diff", ascending=False)
+    largest_diff = df.iloc[0]
+    print(
+        f"Product with largest difference between create and update date: {largest_diff['name']} with {largest_diff['time_diff']} time difference"
+    )
+
+
+def month_with_most_launches(df):
+    """prints month with most launches"""
+    hash_table = {}
+
+    for date in df["createdAt"]:
+        date = date.split("T")[0]
+        month = date.split("-")[1]
+        if month in hash_table:
+            hash_table[month] += 1
+        else:
+            hash_table[month] = 1
+
+    sorted_hash_table = sorted(hash_table.items(), key=lambda x: x[1], reverse=True)
+
+    print("Month with most launches: ", sorted_hash_table[0])
+
+
+def find_correlation_between_tagline_and_votes(df):
+    """prints correlation between tagline and votes"""
+    df["tagline_length"] = df["tagline"].str.len()
+    df["tagline_length"].fillna(0, inplace=True)
+    df["tagline_length"] = df["tagline_length"].astype(int)
+    df["votesCount"] = df["votesCount"].astype(int)
+    print(
+        "correlation between tagline and votes: ",
+        df["tagline_length"].corr(df["votesCount"]),
+    )
+
+
+def find_corr_between_comments_and_votes(df):
+    """prints correlation between comments and votes"""
+    df["commentsCount"] = df["commentsCount"].astype(int)
+    df["votesCount"] = df["votesCount"].astype(int)
+    print(
+        "correlation between comments and votes: ",
+        df["commentsCount"].corr(df["votesCount"]),
+    )
+
+
+def average_votes_and_comments_for_top_10_topics(df):
+    """prints average votes and comments for top 10 topics"""
+    top_df = df.groupby("topic_name").size().sort_values(ascending=False).head(10)
+    top_topics = top_df.index
+    df = df[df["topic_name"].isin(top_topics)]
+    print("Average votes and comments for top 10 topics: ")
+    print(df.groupby("topic_name")[["votesCount", "commentsCount"]].mean())
+
+
+def highest_average_votes_and_comments_for_a_topic(df):
+    """prints highest average votes and comments for a topic"""
+    df = df.groupby("topic_name")[["votesCount", "commentsCount"]].mean()
+    df = df.sort_values(by=["votesCount", "commentsCount"], ascending=False)
+    print("Highest average votes and comments for a topic: ")
+    print(df.head(10))
+
+
+# call the function
+highest_voted_product(df)
+most_commented_product(df)
+top_10_topics(df)
+top_20_words_in_tagline(df)
+dates_with_most_and_least_products(df)
+total_products(df)
+pricing_options_distribution(df)
+comments_per_month(df)
+product_with_most_comments_per_vote(df)
+product_with_largest_difference_between_create_and_update_date(df)
+month_with_most_launches(df)
+find_correlation_between_tagline_and_votes(df)
+find_corr_between_comments_and_votes(df)
+average_votes_and_comments_for_top_10_topics(df)
+highest_average_votes_and_comments_for_a_topic(df)
---
+++ product-fetcher.py
@@ -0,0 +1,265 @@
+import requests
+import pandas as pd
+
+# start timing the execution
+import time
+
+start_time = time.time()
+# Set the GraphQL endpoint and the headers
+endpoint = "https://www.producthunt.com/frontend/graphql"
+headers = {"Content-Type": "application/json"}
+
+# Define the GraphQL query and variables
+query = """
+query ArchivePage(
+	$year: Int
+	$month: Int
+	$day: Int
+	$cursor: String
+	$order: PostsOrder
+) {
+	posts(
+		first: 200
+		year: $year
+		month: $month
+		day: $day
+		order: $order
+		after: $cursor
+	) {
+		edges {
+			node {
+				id
+				...PostItemList
+				__typename
+			}
+			__typename
+		}
+		pageInfo {
+			endCursor
+			hasNextPage
+			__typename
+		}
+		__typename
+	}
+}
+fragment PostItemList on Post {
+	id
+	...PostItem
+	__typename
+}
+fragment PostItem on Post {
+	id
+	commentsCount
+	name
+	shortenedUrl
+	slug
+	tagline
+	updatedAt
+	pricingType
+	topics(first: 1) {
+		edges {
+			node {
+				id
+				name
+				slug
+				__typename
+			}
+			__typename
+		}
+		__typename
+	}
+	redirectToProduct {
+		id
+		slug
+		__typename
+	}
+	...PostThumbnail
+	...PostVoteButton
+	__typename
+}
+fragment PostThumbnail on Post {
+	id
+	name
+	thumbnailImageUuid
+	...PostStatusIcons
+	__typename
+}
+fragment PostStatusIcons on Post {
+	id
+	name
+	productState
+	__typename
+}
+fragment PostVoteButton on Post {
+	id
+	featuredAt
+	updatedAt
+	createdAt
+	disabledWhenScheduled
+	hasVoted
+	... on Votable {
+		id
+		votesCount
+		__typename
+	}
+	__typename
+}
+"""
+
+variables = {
+    "year": 2022,
+    "month": 12,
+    "day": 13,
+    "order": "DAILY_RANK",
+    "cursor": None,
+}
+
+# Initialize an empty list to store the results
+results = []
+
+
+# start_date
+
+start_date = {
+    "year": 2022,
+    "month": 1,
+    "day": 1,
+}
+
+# end_date
+end_date = {
+    "year": 2022,
+    "month": 1,
+    "day": 15,
+}
+
+
+# get_date_range returns a list of dates between the start date and end date
+def get_date_range(start_date, end_date):
+    from datetime import date, timedelta
+
+    start_date = date(start_date["year"], start_date["month"], start_date["day"])
+    end_date = date(end_date["year"], end_date["month"], end_date["day"])
+
+    delta = end_date - start_date
+
+    date_range = []
+    for i in range(delta.days + 1):
+        date_range.append(start_date + timedelta(days=i))
+
+    return date_range
+
+
+for single_date in get_date_range(start_date, end_date):
+    # Set the cursor to None and the hasNextPage flag to True
+    cursor = None
+    hasNextPage = True
+
+    print(single_date.strftime("%Y-%m-%d"))
+    # Fetch the products in a loop, until there are no more pages
+    while hasNextPage:
+        # update the date
+        variables["year"] = single_date.year
+        variables["month"] = single_date.month
+        variables["day"] = single_date.day
+
+        # Update the cursor in the variables
+        variables["cursor"] = cursor
+
+        # Set the GraphQL request payload
+        payload = {"query": query, "variables": variables}
+
+        # Send the POST request to the endpoint
+        response = requests.post(endpoint, json=payload, headers=headers)
+
+        # Check the status code of the response
+        if response.status_code == 200:
+            # Get the data from the response
+            data = response.json()["data"]
+
+            # Extract the list of edges and pageInfo from the data
+            edges = data["posts"]["edges"]
+            pageInfo = data["posts"]["pageInfo"]
+
+            # Extract the cursor and hasNextPage flag from the pageInfo
+            cursor = pageInfo["endCursor"]
+            hasNextPage = pageInfo["hasNextPage"]
+
+            # Iterate over the edges and extract the node from each edge
+            for edge in edges:
+                node = edge["node"]
+
+                # Extract the relevant fields from the node
+                id = node["id"]
+                name = node["name"]
+                slug = node["slug"]
+                tagline = node["tagline"]
+                shortenedUrl = node["shortenedUrl"]
+                commentsCount = node["commentsCount"]
+                updatedAt = node["updatedAt"]
+                pricingType = node["pricingType"]
+
+                # Extract the topic from the node, if it exists
+                topic = (
+                    node["topics"]["edges"][0]["node"]
+                    if node["topics"]["edges"]
+                    else None
+                )
+                topic_id = topic["id"] if topic else None
+                topic_name = topic["name"] if topic else None
+                topic_slug = topic["slug"] if topic else None
+
+                # Extract the redirectToProduct from the node, if it exists
+                redirectToProduct = node["redirectToProduct"]
+                redirectToProduct_id = (
+                    redirectToProduct["id"] if redirectToProduct else None
+                )
+                redirectToProduct_slug = (
+                    redirectToProduct["slug"] if redirectToProduct else None
+                )
+
+                # Extract complete info from PostVoteButton and PostThumbnail
+                featuredAt = node["featuredAt"]
+                createdAt = node["createdAt"]
+                disabledWhenScheduled = node["disabledWhenScheduled"]
+                votesCount = node["votesCount"]
+                productState = node["productState"]
+                thumbnailImageUuid = node["thumbnailImageUuid"]
+
+                # Append the extracted fields to the results list
+                results.append(
+                    {
+                        "id": id,
+                        "name": name,
+                        "slug": slug,
+                        "tagline": tagline,
+                        "shortenedUrl": shortenedUrl,
+                        "commentsCount": commentsCount,
+                        "createdAt": createdAt,
+                        "featuredAt": featuredAt,
+                        "updatedAt": updatedAt,
+                        "pricingType": pricingType,
+                        "topic_id": topic_id,
+                        "topic_name": topic_name,
+                        "topic_slug": topic_slug,
+                        "redirectToProduct_id": redirectToProduct_id,
+                        "redirectToProduct_slug": redirectToProduct_slug,
+                        "disabledWhenScheduled": disabledWhenScheduled,
+                        "votesCount": votesCount,
+                        "productState": productState,
+                        "thumbnailImageUuid": thumbnailImageUuid,
+                    }
+                )
+
+
+# Convert the results list to a Pandas DataFrame
+df = pd.DataFrame(results)
+
+# Write the DataFrame to a CSV file
+df.to_csv("products.csv", index=False)
+
+# end the timer
+end_time = time.time()
+
+# print the time taken to run the script
+print(f"Time taken to run the script: {end_time - start_time} seconds")
